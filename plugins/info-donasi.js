2adfb1f1f6c7386d0d84355d603bf64a436e0a5d
Arwandi00 <100402870+Arwandi00@users.noreply.github.com>
2022-07-05 23:56:57 +0800

    Add files via upload

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,8 +4,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(Telkomsel):*_ 082287219167
-_*Dana/Gopay:*_ 0895612153565
+_*Pulsa/Pulse(Telkomsel):*_ 082195236600 atau 085161098098
+_*Dana/Gopay/Ovo/ShopeePay:*_ 085161098098 atau 082195236600
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
